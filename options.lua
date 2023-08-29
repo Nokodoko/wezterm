45ab2cff56cc30339c559eaa5c75539851b7c117
Nokodoko <cMonty614@gmail.com>
2023-08-28 23:50:55 -0400

    updating general config file

--- options.lua
+++ options.lua
@@ -14,36 +14,6 @@ o.tab_bar = function (bool)
     o.config.enable_tab_bar = bool
 end
 
-o.tab_bar_style = function ()
-    o.config.hide_tab_bar_if_only_one_tab = true
-    o.config.use_fancy_tab_bar = false
-    o.config.text_background_opacity = 0.0
---   local left_arrow =  wez.nerdfonts.pl_right_hard_divider
---   local right_arrow =  wez.nerdfonts.pl_left_hard_divider
-    o.config.tab_bar_style = {
-        new_tab_left = {
-            Background = 'red',
-            Foreground = blue,
-            --{Text = left_arrow},
-        },
-            new_tab_right = wez.format {
-            Background = 'red',
-            Foreground = blue,
-           --{Text = right_arrow},
-        },
-        inactive_tab_left = wez.format{
-            Background = 'red',
-            Foreground = blue,
-            --{Text = left_arrow},
-        },
-        inactive_tab_right = wez.format{
-            Background = 'red',
-            Foreground = blue,
-            --{Text = right_arrow},
-        },
-    }
-end
-
 o.inactive_pane_hsb = function (float1, float2)
     o.config.inactive_pane_hsb = {
         saturation = float1,
@@ -51,8 +21,99 @@ o.inactive_pane_hsb = function (float1, float2)
     }
 end
 
+o.scrollback = function (number)
+    o.config.scrollback_lines = number
+end
+
+o.color_scheme = function (color_scheme)
+   o.config.color_scheme = color_scheme
+end
+
+o.config_colors = function (bg, fg, cbg, cfg, sbg, sfg, sbt, splt, cmab, cmaf, cmib, cmif, qslb, qslf, qsmb, qsmf)
+    o.config.colors = {
+        foreground = fg,
+        background = bg,
+        cursor_fg = cfg,
+        cursor_bg = cbg,
+        selection_fg = sfg,
+        selection_bg = sbg,
+        scrollback_thumb = sbt,
+        split = splt,
+        ansi = {
+            'black',
+            'maroon',
+            'green',
+            'olive',
+            'navy',
+            'purple',
+            'teal',
+            'silver',
+        },
+        brights = {
+            'grey',
+            'red',
+            'lime',
+            'yellow',
+            'blue',
+            'fuchsia',
+            'aqua',
+            'white',
+        },
+    indexed = { [136] = '#af8700' },
+    compose_cursor = 'orange',
+    copy_mode_active_highlight_bg = { Color = cmab },
+    copy_mode_active_highlight_fg = { AnsiColor = cmaf },
+    copy_mode_inactive_highlight_bg = { Color = cmib },
+    copy_mode_inactive_highlight_fg = { AnsiColor = cmif },
+
+    quick_select_label_bg = { Color = qslb },
+    quick_select_label_fg = { Color = qslf },
+    quick_select_match_bg = { AnsiColor = qsmb },
+    quick_select_match_fg = { Color = qsmf },
+    }
+end
+
+o.default_workspace = function (workspace)
+   o.config.default_workspace = workspace
+end
+
 o.font = function (font)
    o.config.font = wez.font_with_fallback(font)
+end
+
+o.tab_bar_style = function (tbsb, tbsf)
+    o.config.hide_tab_bar_if_only_one_tab = true
+    o.config.use_fancy_tab_bar = false
+--   local left_arrow =  wez.nerdfonts.pl_right_hard_divider
+--   local right_arrow =  wez.nerdfonts.pl_left_hard_divider
+    o.config.tab_bar_style = {
+        background = tbsb,
+        foreground = tbsf
+    }
+--        new_tab_left = {
+--            {Background = '#000000'},
+--            --{Background = '#0b0022'},
+--            {Foreground = '#2b2042'},
+--            {Text = left_arrow},
+--        },
+--            new_tab_right = wez.format {
+--            {Background = '#000000'},
+--            --{Background = '#0b0022'},
+--            {Foreground = '#2b2042'},
+--           {Text = right_arrow},
+--        },
+--        inactive_tab_left = wez.format{
+--            {Background = '#000000'},
+--            --{Background = '#0b0022'},
+--            {Foreground = '#2b2042'},
+--            {Text = left_arrow},
+--        },
+--        inactive_tab_right = wez.format{
+--            {Background = '#000000'},
+--            --{Background = '#0b0022'},
+--            {Foreground = '#2b2042'},
+--            {Text = right_arrow},
+--        },
 end
 
 return o
--- wezterm.lua
+++ wezterm.lua
@@ -4,13 +4,44 @@ if not option_status_ok then
 end
 
 --OPTIONS--
-o.opacity(0.8)
+o.opacity(0.95)
+
+--SATURATION, BRIGHTNESS
 o.inactive_pane_hsb(0.9, 0.8)
+
+--DISPLAY TAB BAR
 o.tab_bar(true)
+
+--COLOR SCHEME
+o.color_scheme('Tokyo Night')
+
+o.config_colors({
+--FOREGROUND
+--BACKGROUND
+--CURSOR_FG
+--CURSOR_BG
+--SELECTION_FG
+--SELECTION_BG
+--SCROLLBACK_THUMB
+--SPLIT
+})
+
+--FONT, FONT-FALLBACK
 o.font({
     'VictorMono Nerd Font',
     'Symbol Nerd Font'
 })
-o.tab_bar_style()
+
+o.default_workspace("home")
+
+--SCROLLBACK
+o.scrollback(3000)
+
+o.tab_bar_style({
+    --tab bar background
+    '#0b0022',
+    --tab bar foreground
+    '#2b2042'
+})
 
 return o.config
